cd8030c4a08af1400f9c73fad1bdee060385fc08
drkchiloll <samwomack_@me.com>
2018-03-05 15:45:41 -0600

    added an api sketch

--- src/app/lib/api.ts
+++ src/app/lib/api.ts
@@ -1,14 +1,47 @@
-declare module NodeJS {
-  interface Global {
-    myModule:any;
-  }
-}
-global.myModule = (function() {
-  let service:any = {};
+import axios from 'axios';
 
-  service.init = function() {
-    console.log('I initialized');
+export abstract class Api {
+
+  private static _request(options: any) {
+    return axios(options);
+  }
+
+  static getMeetings() {
+    const options = {
+      url: '/api/meetings',
+      method: 'get'
+    };
+    return this._request(options);
   };
 
-  return service;
-})();
+  static getAudio() {
+    return this._request({
+      url: '/api/volume',
+      method: 'get'
+    });
+  };
+
+  static setAudio(action) {
+    const options = {
+      url: '/api/volume',
+      method: 'post',
+      data: { action }
+    };
+    return this._request(options);
+  };
+
+  static closeConnection() {
+    return this._request({
+      url: '/api/close-connection',
+      method: 'get'
+    });
+  };
+
+  static dial(number) {
+    return this._request({
+      url: '/api/make-call',
+      method: 'post',
+      data: { number }
+    });
+  }
+}

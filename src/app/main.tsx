2744ef1b9288a8608a3cb6fb438f3c0134ab33b4
drkchiloll <samwomack_@me.com>
2018-03-05 15:30:18 -0600

    added material-ui to the project

--- src/app/main.tsx
+++ src/app/main.tsx
@@ -2,4 +2,34 @@ import './vendor';
 import * as React from 'react'
 import * as ReactDom from 'react-dom'
 import { App } from './App';
-ReactDom.render(<App />, document.getElementById('app'));
+
+import { MuiThemeProvider, getMuiTheme, colors } from 'material-ui/styles';
+const { indigo500, indigo200 } = colors;
+const muiTheme = getMuiTheme({
+  palette: {
+    textColor: indigo500,
+    primary1Color: indigo500,
+    primary2Color: indigo500,
+    primary3Color: indigo500
+  },
+  datePicker: {
+    selectColor: indigo500,
+    headerColor: indigo500,
+    calendarTextColor: indigo200
+  },
+  textField: {
+    floatingLabelColor: indigo200,
+    focusColor: indigo200,
+    hintColor: indigo200,
+    textColor: indigo200
+  },
+  fontFamily: `'Times New Roman'`
+});
+
+const Root = () => (
+  <MuiThemeProvider muiTheme={muiTheme}>
+    <App />
+  </MuiThemeProvider>
+);
+
+ReactDom.render(<Root />, document.getElementById('app'));

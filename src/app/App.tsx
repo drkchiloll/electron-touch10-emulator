99bb9a4fa393c22931aa0cdd259dabe2cfa2ad83
drkchiloll <samwomack_@me.com>
2018-03-07 07:00:41 -0600

    integrate Call Component

--- src/app/App.tsx
+++ src/app/App.tsx
@@ -1,12 +1,14 @@
 import * as React from 'react'
 // Import Components
-import { Main, Meetings } from './components';
+import { Main, Meetings, Call } from './components';
 
 export class App extends React.Component<any, any> {
   state = {
     mainView: true,
     meetingsView: false,
-    callView: false
+    callView: false,
+    meeting: null,
+    callId: null
   }
 
   updateView = (args: any) => {
@@ -27,6 +29,14 @@ export class App extends React.Component<any, any> {
         meetingsView: false,
         callView: false
       });
+    } else if(callView) {
+      this.setState({
+        callView,
+        meetingView: false,
+        meetingsView: false,
+        callId: args.callId,
+        meeting: args.meeting
+      });
     }
   }
 
@@ -38,6 +48,8 @@ export class App extends React.Component<any, any> {
           <Main switch={this.updateView} /> :
         meetingsView ?
           <Meetings switch={this.updateView} /> : 
+        callView ?
+          <Call switch={this.updateView} { ...this.state } /> :
           null
       }
     </div>

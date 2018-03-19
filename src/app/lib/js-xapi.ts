d778a118546cca0a0f56a7431a1f2b899d9a5075
drkchiloll <samwomack_@me.com>
2018-03-19 07:28:48 -0500

    updated for restructuring

--- src/app/lib/js-xapi.ts
+++ src/app/lib/js-xapi.ts
@@ -16,14 +16,12 @@ export class JsXAPI {
   public static poller: any;
   public static eventInterval: any;
   public static account: {host, username, password, name, selected};
+  private static reconnectTimeout: any;
 
   static init() {
     return this.connect().then(() => {
       this.poller = () => Promise.all([
-        this.getMeetings(),
-        this.getAudio(),
-        this.getState(),
-        this.getMicStatus()
+        this.getMeetings()
       ]).then(results => {
         this.xapi.emit('update', results);
       });
@@ -36,155 +34,119 @@ export class JsXAPI {
       this.xapi = jsxapi.connect(`ssh://${this.account.host}`, {
         username: this.account.username,
         password: this.account.password,
-        readyTimeout: 2500
+        readyTimeout: 3500
       });
 
       this.xapi.on('ready', () => {
         console.log('we are connected');
+        this.reconnectTimeout = setTimeout(() => {
+          console.log('session timeout');
+          this.xapi.emit('update', 'closing');
+          this.xapi.close();
+          return this.connect();
+        }, 600000);
         return resolve('success');
       });
 
       this.xapi.on('error', (err) => {
-        this.xapi.emit('disconnect-error');
+        this.event.emit('connection-error');
+        this.xapi.removeAllListeners('update');
         return reject(err);
       });
-
-      setTimeout(() => {
-        console.log('session timeout');
-        if(this.xapi) {
-          this.xapi.emit('update', 'closing');
-          this.xapi.close();
-          return this.connect();
-        }
-      }, 600000);
     });
   };
 
-  private static commander(cmd:any) {
+  static commander(cmd:any) {
     // cmd.string = 'the xCommand'
     // cmd.param = Parameters {} of the Command
-    return this.xapi.command(cmd.string, cmd.param);
+    return this.xapi.command(cmd.string, cmd.param).catch(() => {
+      clearTimeout(this.reconnectTimeout);
+      clearInterval(this.eventInterval);
+      this.event.emit('connection-error');
+    });
   };
+
+  static getStatus(status: string) {
+    return this.xapi
+      .status
+      .get(status)
+      .catch(() => this.event.emit('connection-error'));
+  }
 
   static getMeetings() {
     // console.log('getting meetings');
-    if(this.xapi) {
-      return this.commander({
-        string: 'Bookings List',
-        param: {}
-      }).then((bookings:any) => {
-        // console.log(bookings);
-        const { status, ResultInfo: { TotalRows } } = bookings;
-        if(bookings && (parseInt(TotalRows, 10) >= 1)) {
-          return Promise.map(bookings.Booking, (meeting: Booking) => {
-            return {
-              id: meeting.Id,
-              title: meeting.Title,
-              startTime: meeting.Time.StartTime,
-              endTime: meeting.Time.EndTime,
-              endpoint: {
-                number: meeting.DialInfo.Calls.Call[0].Number,
-                type: meeting.DialInfo.Calls.Call[0].CallType
-              }
-            };
-          })
-        } else {
-          return [];
-        }
-      })
-    } else {
-      return this.connect().then(() => this.getMeetings());
-    }
+    return this.commander({
+      string: 'Bookings List',
+      param: {}
+    }).then((bookings:any) => {
+      const { status, ResultInfo: { TotalRows } } = bookings;
+      if(bookings && (parseInt(TotalRows, 10) >= 1)) {
+        return Promise.map(bookings.Booking, (meeting: Booking) => {
+          return {
+            id: meeting.Id,
+            title: meeting.Title,
+            startTime: meeting.Time.StartTime,
+            endTime: meeting.Time.EndTime,
+            endpoint: {
+              number: meeting.DialInfo.Calls.Call[0].Number,
+              type: meeting.DialInfo.Calls.Call[0].CallType
+            }
+          };
+        })
+      } else {
+        return [];
+      }
+    })
   };
 
   static getAudio() {
-    // console.log('retrieve audio volume');
-    if(this.xapi) {
-      return this.xapi.status.get('Audio Volume');
-    } else {
-      return this.connect().then(() => this.getAudio());
-    }
+    return this.getStatus('Audio Volume');
   };
 
   static getMicStatus() {
-    if(this.xapi) {
-      // Returns On/Off
-      return this.xapi.status.get('Audio Microphones Mute');
-    } else {
-      return this.connect().then(() => this.getMicStatus());
-    }
+    return this.getStatus('Audio Microphones Mute');
   }
 
   static getState() {
-    // Standby, EnteringStandby, Halfwake, Off(not in standby)
-    if(this.xapi) {
-      return this.xapi.status.get('Standby State');
-    } else {
-      return this.connect().then(() => this.getState());
-    }
+    return this.getStatus('Standby State');
   };
 
   static setAudio(action) {
-    if(this.xapi) {
-      return this.commander({
-        string: `Audio Volume ${action}`,
-        param: { Steps: 1 }
-      })
-    } else {
-      return this.connect().then(() => this.setAudio(action));
-    }
+    return this.commander({
+      string: `Audio Volume ${action}`,
+      param: { Steps: 1 }
+    });
   };
 
   // action: String; Mute | Unmute
   static setMic(action) {
-    if(this.xapi) {
-      return this.commander({
-        string: `Audio Microphones ${action}`,
-        param: {}
-      });
-    } else {
-      return this.connect().then(() => this.setMic(action));
-    }
+    return this.commander({
+      string: `Audio Microphones ${action}`,
+      param: {}
+    });
   }
 
   static closeConnection() {
-    if(this.xapi) {
-      this.xapi.close();
-      this.xapi = null;
-    }
+    this.xapi.close();
     return Promise.resolve();
   };
 
   static dial(Number) {
-    if(this.xapi) {
-      return this.commander({
-        string: 'Dial',
-        param: { Number }
-      });
-    } else {
-      return this.connect().then(() =>
-        this.dial(Number));
-    }
+    return this.commander({
+      string: 'Dial',
+      param: { Number }
+    });
   };
 
   static hangUp(CallId) {
-    if(this.xapi) {
-      return this.commander({
-        string: 'Call Disconnect',
-        param: { CallId }
-      });
-    } else {
-      return this.connect().then(() => this.hangUp(CallId));
-    }
+    return this.commander({
+      string: 'Call Disconnect',
+      param: { CallId }
+    });
   };
 
   static wakeStatus() {
-    if(this.xapi) {
-      return this.xapi.status
-        .get('Standby State');
-    } else {
-      return this.connect().then(() => this.wakeStatus());
-    }
+    return this.getStatus('Standby State');
   };
 
   static updateWakeStatus(status: string) {
@@ -192,100 +154,91 @@ export class JsXAPI {
     // Activate: (sets the display to Standby)
     // Deactivate: (turns the display on)
     // Halfwake: (Shows message on display to "Touch to Wake")
-    // ResetTimer
-    if(this.xapi) {
-      return this.commander({
-        string: `Standby ${status}`,
-        param: {}
-      });
-    } else {
-      return this.connect().then(() => this.updateWakeStatus(status));
-    }
+    return this.commander({
+      string: `Standby ${status}`,
+      param: {}
+    });
   };
 
   static getDirectory({query = ''}) {
-    if(this.xapi) {
-      return this.commander({
-        string: 'Phonebook Search',
-        param: {
-          PhonebookId: 'default',
-          PhonebookType: 'Corporate',
-          SearchString: query,
-          Limit: 65534
-        }
-      }).then(({ Contact }) => {
-        if(!Contact) {
-          return [];
-        } else {
-          return Promise.reduce(Contact, (a, u: any) => {
-            if(u.Name === 'user') return a;
-            let user: any = {
-              id: u.id,
-              name: u.Name
-            };
-            if(u.ContactMethod && u.ContactMethod.length > 0) {
-              return Promise.map(u.ContactMethod, ({ Number }) => {
-                return { number: Number };
-              }).then(contacts => {
-                user['contacts'] = contacts;
-                a.push(user);
-                return a;
-              });
-            } else {
+    return this.commander({
+      string: 'Phonebook Search',
+      param: {
+        PhonebookId: 'default',
+        PhonebookType: 'Corporate',
+        SearchString: query,
+        Limit: 65534
+      }
+    }).then(({ Contact }) => {
+      if(!Contact) {
+        return [];
+      } else {
+        return Promise.reduce(Contact, (a, u: any) => {
+          if(u.Name === 'user') return a;
+          let user: any = {
+            id: u.id,
+            name: u.Name
+          };
+          if(u.ContactMethod && u.ContactMethod.length > 0) {
+            return Promise.map(u.ContactMethod, ({ Number }) => {
+              return { number: Number };
+            }).then(contacts => {
+              user['contacts'] = contacts;
+              a.push(user);
               return a;
-            }
-          }, []);
-        }
-      });
-    }
+            });
+          } else {
+            return a;
+          }
+        }, []);
+      }
+    });
   };
 
   static getCallHistory({ query = '' }) {
-    if(this.xapi) {
-      return this.commander({
-        string: 'CallHistory Get',
-        param: {
-          Filter: 'All',
-          Limit: 65534,
-          DetailLevel: 'Full',
-          SearchString: query
-        }
-      }).then(({ Entry }) => {
-        return Promise.map(Entry, (history: any) => {
-          return {
-            number: history.CallbackNumber,
-            startTime: history.StartTime,
-            duration: parseInt(history.Duration, 10) / 60,
-            roomCount: history.RoomAnalytics.PeopleCount,
-            callQuality: {
-              audio: {
-                incoming: {
-                  maxJitter: history.Audio.Incoming.MaxJitter,
-                  packetLoss: history.Audio.Incoming.PacketLoss,
-                  packetLossPercent: history.Audio.Incoming.PacketLossPercentage
-                },
-                outgoing: {
-                  maxJitter: history.Audio.Outgoing.MaxJitter,
-                  packetLoss: history.Audio.Outgoing.PacketLoss,
-                  packetLossPercent: history.Audio.Outgoing.PacketLossPercentage
-                }
+    return this.commander({
+      string: 'CallHistory Get',
+      param: {
+        Filter: 'All',
+        Limit: 65534,
+        DetailLevel: 'Full',
+        SearchString: query
+      }
+    }).then(({ Entry }) => {
+      return Promise.map(Entry, (history: any) => {
+        return {
+          number: history.CallbackNumber,
+          startTime: history.StartTime,
+          duration: parseInt(history.Duration, 10) / 60,
+          roomCount: history.RoomAnalytics.PeopleCount,
+          callQuality: {
+            audio: {
+              incoming: {
+                maxJitter: history.Audio.Incoming.MaxJitter,
+                packetLoss: history.Audio.Incoming.PacketLoss,
+                packetLossPercent: history.Audio.Incoming.PacketLossPercentage
               },
-              video: {
-                incoming: {
-                  maxJitter: history.Video.Incoming.MaxJitter,
-                  packetLoss: history.Video.Incoming.PacketLoss,
-                  packetLossPercent: history.Video.Incoming.PacketLossPercentage
-                },
-                outgoing: {
-                  maxJitter: history.Video.Outgoing.MaxJitter,
-                  packetLoss: history.Video.Outgoing.PacketLoss,
-                  packetLossPercent: history.Video.Outgoing.PacketLossPercentage
-                }
+              outgoing: {
+                maxJitter: history.Audio.Outgoing.MaxJitter,
+                packetLoss: history.Audio.Outgoing.PacketLoss,
+                packetLossPercent: history.Audio.Outgoing.PacketLossPercentage
+              }
+            },
+            video: {
+              incoming: {
+                maxJitter: history.Video.Incoming.MaxJitter,
+                packetLoss: history.Video.Incoming.PacketLoss,
+                packetLossPercent: history.Video.Incoming.PacketLossPercentage
+              },
+              outgoing: {
+                maxJitter: history.Video.Outgoing.MaxJitter,
+                packetLoss: history.Video.Outgoing.PacketLoss,
+                packetLossPercent: history.Video.Outgoing.PacketLossPercentage
               }
             }
-          };
-        });
+          }
+        };
       });
-    }
+    });
   };
 }

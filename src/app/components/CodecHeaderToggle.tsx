4e5f970844359131e391a5d800c34d99c749a201
drkchiloll <samwomack_@me.com>
2018-05-11 08:41:54 -0500

    enhanced in order to handle Account switching more efficiently..

--- src/app/components/CodecHeaderToggle.tsx
+++ src/app/components/CodecHeaderToggle.tsx
@@ -2,6 +2,7 @@ import * as React from 'react';
 import { IconButton, IconMenu, MenuItem } from 'material-ui';
 import MoreVertIcon from 'material-ui/svg-icons/navigation/more-vert';
 import IsConnectedIcon from 'material-ui/svg-icons/av/fiber-manual-record';
+import { Accounts } from '../lib';
 
 const styles: any = {
   menu: { position: 'absolute', top: 0, width: 30 },
@@ -20,40 +21,72 @@ const styles: any = {
   }
 };
 
-export function CodecHeaderToggle(props: any) {
-  const { accounts, account, connected } = props;
-  return (
-    <div>
-      <IconMenu
-        style={styles.menu}
-        anchorOrigin={styles.menuorigin}
-        targetOrigin={styles.menuorigin}
-        onItemClick={props.change}
-        menuStyle={{maxHeight: 600, overflow: 'auto' }}
-        iconButtonElement={
-          <IconButton tooltip='Toggle Codecs'
-            tooltipPosition='bottom-right'
-          > <MoreVertIcon /> </IconButton>
-        } >
-        { accounts.sort((a:any,b:any) => {
+export class CodecHeaderToggle extends React.Component<any,any> {
+  constructor(props) {
+    super(props);
+    this.state = {
+      accounts: null,
+      account: null
+    };
+  }
+
+  componentWillMount() {
+    let accts = Accounts.get();
+    let selacct = accts.find(a => a.selected);
+    this.setState({
+      accounts: accts,
+      account: selacct
+    });
+  }
+
+  changeAccount = (account) => {
+    let { accounts } = this.state;
+    accounts.forEach(a => {
+      if(a.selected) a.selected = false;
+    });
+    accounts.find(a => a.name == account.name).selected = true;
+    Accounts.save(accounts);
+    this.props.change(account);
+    this.setState({ accounts, account });
+  };
+
+  render() {
+    let { connected } = this.props;
+    let { accounts, account } = this.state;
+    return (
+      <div>
+        <IconMenu
+          style={styles.menu}
+          anchorOrigin={styles.menuorigin}
+          targetOrigin={styles.menuorigin}
+          onItemClick={(e: any, {props: {value}}) =>
+            this.changeAccount(value)}
+          menuStyle={{ maxHeight: 600, overflow: 'auto' }}
+          iconButtonElement={
+            <IconButton tooltip='Toggle Codecs'
+              tooltipPosition='bottom-right'
+            > <MoreVertIcon /> </IconButton>
+          } >
+          {accounts.sort((a: any, b: any) => {
             if(a.name > b.name) return -1;
             if(a.name < b.name) return 1;
             return 0
           })
-          .map((a: any, i: number) =>
-            <MenuItem value={a}
-              key={`account_${i}`}
-              primaryText={a.name} />) }
-      </IconMenu>
-      <p
-        style={{
-          font: '14px arial', color: 'grey', width: 600, marginLeft: '40px',
-          marginTop: '16px'
-        }}>
-        {account.name}>
+            .map((a: any, i: number) =>
+              <MenuItem value={a}
+                key={`account_${i}`}
+                primaryText={a.name} />)}
+        </IconMenu>
+        <p
+          style={{
+            font: '14px arial', color: 'grey', width: 600, marginLeft: '40px',
+            marginTop: '16px'
+          }}>
+          {account.name}>
         <IsConnectedIcon style={{ position: 'absolute', top: 12, marginLeft: '2px' }}
-          color={connected ? 'green' : 'red'} />
-      </p>
-    </div>
-  );
+            color={connected ? 'green' : 'red'} />
+        </p>
+      </div>
+    );
+  }
 }

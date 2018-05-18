c6337463b510d57770272e69a8cff1e8f742d277
drkchiloll <samwomack_@me.com>
2018-05-18 16:49:18 -0500

    refined Camera Controls..except MX300 (probably on old code)

--- src/app/components/CamControl.tsx
+++ src/app/components/CamControl.tsx
@@ -49,9 +49,10 @@ export class JoyStick extends React.Component<any,any> {
         if(angle <= 200 && angle >= 155.001) return 180;
         if(angle <= 245 && angle >= 200.001) return 225;
         if(angle <= 290 && angle >= 245.001) return 270;
+        if(angle <= 335 && angle >= 290.001) return 315;
         if(angle <= 315 && angle >= 290.001) return 270; // 315
         if(angle <= 335 && angle >= 315.001) return 0; // 315
-        if(angle <= 359.999 && angle >= 335.001) return 0;
+        // if(angle <= 359.999 && angle >= 335.001) return 0;
       };
       const dir = (angle: number) => {
         const { cameraId } = this.state;
@@ -59,41 +60,66 @@ export class JoyStick extends React.Component<any,any> {
           CameraId: cameraId
         };
         if(angle === 0) {
-          return Object.assign(params, { PanSpeed: 1, Pan: 'Right' });
+          return Object.assign(params, { Pan: 'Right', PanSpeed: 1 });
         } else if(angle === 45) {
           return Object.assign(params, {
-            PanSpeed: 1, TiltSpeed: 1, Pan: 'Right', Tilt: 'Up'
+            Pan: 'Right', PanSpeed: 1, Tilt: 'Up', TiltSpeed: 1
           });
         } else if(angle === 90) {
-          return Object.assign(params, { TiltSpeed: 1, Tilt: 'Up' });
+          return Object.assign(params, { Tilt: 'Up', TiltSpeed: 1 });
         } else if(angle === 135) {
           return Object.assign(params, {
-            PanSpeed: 1, TiltSpeed: 1, Tilt: 'Up', Pan: 'Left'
+            Pan: 'Left', PanSpeed: 1, Tilt: 'Up', TiltSpeed: 1
           });
         } else if(angle === 180) {
-          return Object.assign(params, { PanSpeed: 1, Pan: 'Left' });
+          return Object.assign(params, { Pan: 'Left', PanSpeed: 1 });
         } else if(angle === 225) {
           return Object.assign(params, {
-            PanSpeed: 1, TiltSpeed: 1, Tilt: 'Down', Pan: 'Left'
+            Pan: 'Left', PanSpeed: 1, Tilt: 'Down', TiltSpeed: 1
           });
         } else if(angle === 270) {
-          return Object.assign(params, { TiltSpeed: 1, Tilt: 'Down' });
+          return Object.assign(params, { Tilt: 'Down', TiltSpeed: 1 });
+        } else if(angle === 315) {
+          return Object.assign(params, {
+            Pan: 'Right', PanSpeed: 1, Tilt: 'Down', TiltSpeed: 1
+          })
         }
       };
       let { position, moving } = this.state;
       let { angle } = stick;
-      let deg = angle.degree,
-        newpos = angleChange(deg),
-        direction = dir(newpos);
-      if(!position) position = newpos;
+      let deg = angle.degree;
+      let newpos = angleChange(deg);
+      let direction = dir(newpos);
+      if(position === null) {
+        position = newpos;
+        this.setState({ position });
+      }
+      if(!direction) return;
       if(position === newpos) {
-        this.setState({ position: newpos });
         if(!moving) {
-          JsXAPI.commander({ cmd: 'Camera Ramp', params: direction })
-            .then(() => this.setState({ moving: true}));
+          this.setState({ moving: true })
+          JsXAPI.commander({
+            cmd: 'Camera Ramp', params: direction
+          });
         }
       } else {
         this.setState({ moving: false, position: newpos });
+        setTimeout(() => {
+          JsXAPI.commander({
+            cmd: 'Camera Ramp',
+            params: {
+              CameraId: this.state.cameraId,
+              Pan: 'Stop',
+              Tilt: 'Stop'
+            }
+          })
+        }, 0);
+      }
+    });
+
+    manager.on('end', () => {
+      this.setState({ position: -1, moving: false });
+      setTimeout(() => {
         JsXAPI.commander({
           cmd: 'Camera Ramp',
           params: {
@@ -102,19 +128,7 @@ export class JoyStick extends React.Component<any,any> {
             Tilt: 'Stop'
           }
         })
-      }
-    });
-
-    manager.on('end', () => {
-      JsXAPI.commander({
-        cmd: 'Camera Ramp',
-        params: {
-          CameraId: this.state.cameraId,
-          Pan: 'Stop',
-          Tilt: 'Stop'
-        }
-      })
-      this.setState({ moving: false });
+      }, 0);
     })
     const front: any = document.querySelector('.front');
     front.style.backgroundImage = `url(${WebExTeams})`;

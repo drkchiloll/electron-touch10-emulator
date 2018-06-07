03e26a8a5712236886c88d92ed9d5d268a5ce37f
drkchiloll <samwomack_@me.com>
2018-06-07 07:09:30 -0500

    updated for Webpack version 4

--- webpack.config.js
+++ webpack.config.js
@@ -1,7 +1,7 @@
 const path = require('path'),
   fs = require('fs'),
   webpack = require('webpack'),
-  merge = require('webpack-merge'),
+  merge = require('webpack-merge')
   UglifyJsPlugin = require('uglifyjs-webpack-plugin');
 
 const { NODE_ENV } = process.env;
@@ -15,68 +15,67 @@ fs.readdirSync('node_modules')
     nodeModules[mod] = 'commonjs ' + mod;
   });
 
-let dev = {
-  entry: {
-    main: path.join(__dirname, 'src/electron-main/main.ts'),
-    app: path.join(__dirname, 'src/app/main.tsx')
-  },
-  output: {
-    path: path.join(__dirname, 'dist'),
-    filename: '[name].bundle.js'
-  },
-  target: 'electron-main',
-  node: {
-    __dirname: false,
-    console: true,
-    fs: 'empty',
-    net: 'empty',
-    tls: 'empty'
-  },
-  devtool: 'source-map',
-  resolve: {
-    extensions: ['.ts', '.tsx', '.js']
-  },
-  module: {
-    rules: [{
-      test: /\.tsx?$/,
-      loader: "awesome-typescript-loader",
-      exclude: /(node_modules|server.js)/,
-      options: {
-        transpileOnly: false
-      }
-    }, {
-      test: /\.css$/,
-      use: ['style-loader', {
-        loader: 'css-loader',
+module.exports = (env, options) => {
+  let dev = {
+    entry: {
+      main: path.join(__dirname, 'src/electron-main/main.ts'),
+      app: path.join(__dirname, 'src/app/main.tsx')
+    },
+    output: {
+      path: path.join(__dirname, 'dist'),
+      filename: '[name].bundle.js'
+    },
+    target: 'electron-main',
+    node: {
+      __dirname: false,
+      console: true,
+      fs: 'empty',
+      net: 'empty',
+      tls: 'empty'
+    },
+    devtool: 'source-map',
+    resolve: {
+      extensions: ['.ts', '.tsx', '.js']
+    },
+    module: {
+      rules: [{
+        test: /\.tsx?$/,
+        loader: 'awesome-typescript-loader',
         options: {
-          modules: true
+          transpileOnly: false
         }
+      }, {
+        test: /\.css$/,
+        use: ['style-loader', 'css-loader']
+      }, {
+          test: /.woff$|.woff2$|.ttf$|.eot$|.svg$|.jpg$/,
+        loader: 'url-loader'
       }]
-    }, {
-      test: /.woff$|.woff2$|.ttf$|.eot$|.svg$|.jpg$/,
-      loader: 'url-loader'
-    }]
-  }
-  // externals: nodeModules,
-};
+    }
+  };
 
-let prod = {
-  plugins: [
-    new UglifyJsPlugin(),
-    new webpack.DefinePlugin({
-      "process.env": {
-        NODE_ENV: JSON.stringify("production")
-      }
-    }),
-    new webpack.LoaderOptionsPlugin({
-      minimize: true,
-      compress: {
-        warnings: false,
-        drop_console: false,
-      }
-    })
-  ]
+  let prod = {
+    plugins: [
+      new UglifyJsPlugin({
+        cache: false,
+        uglifyOptions: {
+          compress: { inline: false }
+        }
+      }),
+      new webpack.DefinePlugin({
+        'process.env': {
+          NODE_ENV: JSON.stringify('production')
+        }
+      }),
+      new webpack.LoaderOptionsPlugin({
+        minimize: true,
+        compress: {
+          warnings: false,
+          drop_console: false
+        }
+      })
+    ]
+  };
+  if (options.mode === 'production') return merge(dev, prod)
+  else return dev;
 };
-
-if (NODE_ENV === 'production') module.exports = merge(dev, prod);
-else module.exports = dev;
